2c83536b44816877a7df693e04f89ccbb5c4161c
joycecz1412 <149197895+joycecz1412@users.noreply.github.com>
2024-10-09 13:04:33 -0500

    Update samplit-username.py

--- samplit-username.py
+++ samplit-username.py
@@ -2,5 +2,9 @@
 random number of sample lines in file'''
 
 def sample_username(file_name):
+	with open('file.csv', mode='r') as file:
+		for line in file:
+        	line = line.strip()
+        	values = line.split(',')
 	return
 
